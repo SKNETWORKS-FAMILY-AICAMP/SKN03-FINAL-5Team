0dbe023bd92e6fc844dde102fda52eb22161f111
Lerini98 <hyerin7259@gmail.com>
2024-11-22 18:59:50 +0900

    build : page.js import path update

--- frontend/app/myPage/page.js
+++ frontend/app/myPage/page.js
@@ -1,8 +1,16 @@
-import Container from '../common/components/container';
-import Header from '../common/components/header';
+'use client'; // 클라이언트 컴포넌트 선언 필요
+
+import Container from '@/common/components/container';
+import Header from '@/common/components/header';
 import InterviewHistory from './components/interviewHistory';
 import SideNavigation from './components/navigation';
 import { Flex, Box } from '@chakra-ui/react';
+
+// import Container from '../common/components/container';
+// import Header from '../common/components/header';
+// import InterviewHistory from './components/interviewHistory';
+// import SideNavigation from './components/navigation';
+// import { Flex, Box } from '@chakra-ui/react';
 
 function MyPages() {
   return (
