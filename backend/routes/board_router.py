a7c17c942d593a6813d7e2104a6c163671b8b346
“Lerini98” <hyerin7259@gmail.com>
2024-11-27 17:50:14 +0900

    feat : crud setting

--- backend/routes/board_router.py
+++ backend/routes/board_router.py
@@ -1,11 +1,13 @@
 from sqlalchemy.orm import Session
 from .auth import get_db
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from routes import board_crud, board_schema
 
 router = APIRouter(
     prefix = "/board"
 )
 
-@router.get("/test")
-async def board_test():
-    return "test"
+@router.post("/create", description="기본 게시판 - 게시글 생성")
+async def create_new_post(new_post : board_schema.NewPost, db : Session):
+    return board_crud.insert_post(new_post, db)
---
+++ backend/routes/board_schema.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class NewPost(BaseModel):
+    writer : str
+    title : str
+    content : Optional[str] = None
