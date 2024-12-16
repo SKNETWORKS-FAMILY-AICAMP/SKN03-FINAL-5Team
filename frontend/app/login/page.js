424f0cace1e368c7a2589667d5f551e0d6c90099
min3009 <min3009@naver.com>
2024-12-16 19:07:41 +0900

    test

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -6,6 +6,17 @@ import React from 'react';
 import { getParameterStore } from '../common/utils/getParameterStore';
 
 async function LoginPage() {
+  const KAKAO_REST_API_KEY = await getParameterStore(
+    '/interviewdb-info/kakao/RESTAPI'
+  );
+  const KAKAO_REDIRECT_URI = await getParameterStore(
+    '/interviewdb-info/kakao/KAKAO_REDIRECT_URI'
+  );
+
+  if (!KAKAO_REST_API_KEY || !KAKAO_REDIRECT_URI) {
+    console.error('Failed to fetch one or more parameters.');
+  }
+
   return (
     <Container>
       <Header />
@@ -15,7 +26,10 @@ async function LoginPage() {
         alignItems="center"
         justifyContent="center"
       >
-        <Login />
+        <Login
+          KAKAO_REST_API_KEY={KAKAO_REST_API_KEY}
+          KAKAO_REDIRECT_URI={KAKAO_REDIRECT_URI}
+        />
       </Box>
     </Container>
   );
