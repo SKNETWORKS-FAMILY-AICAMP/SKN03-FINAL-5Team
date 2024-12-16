42380a132444d5f23243cce6865e4a4e88eceabf
min3009 <min3009@naver.com>
2024-12-16 19:20:52 +0900

    fix

--- frontend/app/common/utils/getParameterStore.js
+++ frontend/app/common/utils/getParameterStore.js
@@ -1,33 +1,27 @@
 import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm';
-import { fromContainerMetadata } from '@aws-sdk/credential-providers';
 
-const client = new SSMClient({
-  region: process.env.AWS_REGION || 'ap-northeast-2',
-  credentials: fromContainerMetadata({
-    maxRetries: 3, // Optional
-    timeout: 0, // Optional
-  }),
-});
-
-export async function getParameterStore(parameterName) {
-  console.log(`Fetching parameter: ${parameterName}`);
-  const command = new GetParameterCommand({
-    Name: parameterName,
-    WithDecryption: true,
+/**
+ * Fetches a parameter value from AWS SSM Parameter Store.
+ *
+ * @param {string} parameterName - The name of the parameter to fetch.
+ * @returns {Promise<string>} - The value of the parameter.
+ */
+export async function getParameterValue(parameterName) {
+  const client = new SSMClient({
+    region: process.env.AWS_REGION, // Ensure the region is correctly set in your environment
   });
 
   try {
+    const command = new GetParameterCommand({
+      Name: parameterName,
+      WithDecryption: true,
+    });
+
     const response = await client.send(command);
-    console.log('Full response:', JSON.stringify(response, null, 2)); // 응답 로그 추가
-    if (response.Parameter && response.Parameter.Value) {
-      console.log('Parameter fetched successfully:', response.Parameter.Value);
-      return response.Parameter.Value;
-    } else {
-      throw new Error('No value found for the parameter');
-    }
+
+    return response.Parameter?.Value || '';
   } catch (error) {
-    console.error('Error fetching parameter:', error.message);
-    console.error('Full error:', JSON.stringify(error, null, 2));
-    return null;
+    console.error(`Error fetching parameter "${parameterName}":`, error);
+    throw error;
   }
 }
