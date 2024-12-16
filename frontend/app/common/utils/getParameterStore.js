aca117b39e59f6b8a0818cae9f14f3e7a05e9ced
min3009 <min3009@naver.com>
2024-12-16 18:28:25 +0900

    test

--- frontend/app/common/utils/getParameterStore.js
+++ frontend/app/common/utils/getParameterStore.js
@@ -1,7 +1,12 @@
 import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm';
+import { fromContainerMetadata } from '@aws-sdk/credential-providers';
 
-const ssmClient = new SSMClient({
+const client = new SSMClient({
   region: process.env.AWS_REGION || 'ap-northeast-2',
+  credentials: fromContainerMetadata({
+    maxRetries: 3, // Optional
+    timeout: 0, // Optional
+  }),
 });
 
 export async function getParameterStore(parameterName) {
@@ -12,10 +17,14 @@ export async function getParameterStore(parameterName) {
   });
 
   try {
-    const response = await ssmClient.send(command);
+    const response = await client.send(command);
     console.log('Full response:', JSON.stringify(response, null, 2)); // 응답 로그 추가
-    console.log('Parameter fetched successfully:', response.Parameter.Value);
-    return response.Parameter.Value;
+    if (response.Parameter && response.Parameter.Value) {
+      console.log('Parameter fetched successfully:', response.Parameter.Value);
+      return response.Parameter.Value;
+    } else {
+      throw new Error('No value found for the parameter');
+    }
   } catch (error) {
     console.error('Error fetching parameter:', error.message);
     console.error('Full error:', JSON.stringify(error, null, 2));
