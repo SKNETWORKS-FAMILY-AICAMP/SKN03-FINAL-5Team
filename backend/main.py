9613aa515ca2bcaac65b180b52a10f75f0dc6661
min3009 <min3009@naver.com>
2024-12-17 09:16:35 +0900

    fix

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     # allow_origins=["http://localhost:3000"],
-    allow_origins=["https://api.unailit.com"],
+    allow_origins=["https://api.aiunailit.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
